eb154825d69b6e4bbaf42e23b15bbfcc2372ce14
ujjwalk <ujjwalkeshri@hotmail.com>
2024-07-30 22:36:53 +0530

    first commit

---
+++ src/test/java/com/uk/TempDemo/TempDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uk.TempDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
